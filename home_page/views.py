23cb7ab37a3c72a73c530e94d3016fed651fa5b0
leogutschow <leogusalles@hotmail.com>
2022-07-29 21:34:25 -0300

    Criating a Initial Home Page the Shows the list of monsters there is in the DB

--- home_page/views.py
+++ home_page/views.py
@@ -1,6 +1,17 @@
+from django.db.models import QuerySet
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from monsters.models import Monster
+
 
 # Create your views here.
 class Index(TemplateView):
+    model = Monster
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context: dict = super().get_context_data()
+        monsters: Monster = Monster.objects.all()
+        context["monsters"]: QuerySet = monsters
+        print(monsters)
+        return context
