72e91014d1f97643945f80a06a6801b0f0e276c7
leogutschow <leogusalles@hotmail.com>
2022-07-29 21:37:28 -0300

    Creating Monster Model and Admin

--- monsters/admin.py
+++ monsters/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Monster
 
 # Register your models here.
+class MonsterAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'ac', 'hp', 'challenge',
+    )
+
+
+admin.site.register(Monster, MonsterAdmin)
--- monsters/models.py
+++ monsters/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class BaseSheet(models.Model):
+    name: str = models.CharField(max_length=50)
+    race: str = models.CharField(max_length=30)
+    size: str = models.CharField(max_length=30)
+    ac: int = models.IntegerField()
+    ac_type: str = models.CharField(max_length=20)
+    hp: int = models.IntegerField()
+    hp_dices: str = models.CharField(max_length=10)
+    movement: str = models.CharField(max_length=30)
+    strength: int = models.IntegerField()
+    dexterity: int = models.IntegerField()
+    constitution: int = models.IntegerField()
+    intelligence: int = models.IntegerField()
+    wisdom: int = models.IntegerField()
+    charisma: int = models.IntegerField()
+    slug: str = models.SlugField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f'{self.name}-{self.id}')
+        return super().save()
+
+
+class Monster(BaseSheet):
+    challenge: str = models.CharField(default="0", max_length=3)
+    description: str = models.TextField(default="")
