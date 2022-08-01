1ee932a1e9666912edad5ef234b4c9ab5b07d274
leogutschow <leogusalles@hotmail.com>
2022-08-01 18:52:08 -0300

    COnfigurando Primeito Deploy

--- MonsterImporter/settings.py
+++ MonsterImporter/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
