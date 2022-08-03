d6315f5827933f40c507152a7de9bda6ef4a932b
leogutschow <leogusalles@hotmail.com>
2022-08-03 19:08:48 -0300

    Criando primeiro link de redirecionamento para a Lista de Monstros

--- monsters/urls.py
+++ monsters/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import MonsterDetail
+from .views import MonsterDetail, MonsterList
+
+app_name = 'monster'
 
 urlpatterns: list = [
-    path('<str:slug>', MonsterDetail.as_view())
+    path('<str:slug>', MonsterDetail.as_view(), name="monster_detail"),
+    path('all', MonsterList.as_view(), name='monster_list')
 ]
