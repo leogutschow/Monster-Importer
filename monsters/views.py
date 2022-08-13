f781e528b69b147dd6e6189a8ad0efe1b85066b8
leogutschow <leogusalles@hotmail.com>
2022-08-13 11:03:00 -0300

    Colocando um dicionario do Monstro no contexto para ele ser traduzido em um JSON

--- monsters/views.py
+++ monsters/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from django.views.generic import DetailView, ListView
-from .models import Monster
+from .models import Monster, Action, SpecialTraits
 from django.core.paginator import Paginator
 from django.template.loader import render_to_string
 from django.http import JsonResponse
+
 
 # Create your views here.
 class MonsterDetail(DetailView):
@@ -12,8 +14,22 @@ class MonsterDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context: dict = super().get_context_data()
-        monster = self.get_object()
+        monster: Monster = self.get_object()
         context['monster'] = monster
+        # Transforming the monster in a Dict so it can be passed as a JSON object in the Template and adding the Actions
+        # and Special Traits to the Dict
+        monster_dict: dict = model_to_dict(monster)
+        monster_dict.pop('image')
+        monster_actions: dict = {}
+        monster_special_traits: dict = {}
+        for num, action in enumerate(Action.objects.filter(monster=monster.pk).values()):
+            monster_actions['action' + str(num)] = action
+        for num, special_trait in enumerate(SpecialTraits.objects.filter(monster=monster.pk).values()):
+            monster_special_traits['special_trait' + str(num)] = special_trait
+        monster_dict['special_traits'] = monster_special_traits
+        monster_dict['actions'] = monster_actions
+        context['monster_dict'] = monster_dict
+
         return context
 
 
@@ -47,4 +63,3 @@ class MonsterList(ListView):
             qs = qs.filter(challenge__icontains=monster_challenge_query)
         context['monsters'] = qs
         return render(request, template_name=self.template_name, context=context)
-
