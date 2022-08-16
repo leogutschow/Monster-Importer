6f29407c3aafe03149a99d21d90dc9ffd9c5a664
leogutschow <leogusalles@hotmail.com>
2022-08-16 15:41:48 -0300

    Criando Model e Forms para os Requests dos Clientes

--- client_requests/views.py
+++ client_requests/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
+from .forms import FormRequest
 
 
 # Create your views here.
 class NewRequest(CreateView):
     template_name = 'client_requests/new_request.html'
+    form_class = FormRequest
