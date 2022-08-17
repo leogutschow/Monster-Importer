1ca65e003dff0bc03f43a65eca6d14375ed15bd4
leogutschow <leogusalles@hotmail.com>
2022-08-17 18:21:36 -0300

    Criando Login, Logout e Register Views e Urls

--- authentications/views.py
+++ authentications/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import FormView, RedirectView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 # Create your views here.
+class Login(LoginView):
+    pass
+
+
+class Logout(LogoutView):
+    pass
+
+
+class Register(FormView):
+    pass
