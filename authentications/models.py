69248982106c80f8f66c110c88b4b1de3aa1be18
leogutschow <leogusalles@hotmail.com>
2022-08-28 10:44:23 -0300

    Adicionando um campo da data da criaçao do perfil nos Profiles

--- authentications/models.py
+++ authentications/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User, AbstractUser
+from datetime import datetime
 
 
 # Create your models here.
@@ -11,6 +12,7 @@ class Profile(models.Model):
     image: str = models.ImageField(upload_to=f'images/profiles/{user}', blank=True, null=True)
     motto: str = models.CharField(max_length=100, blank=True, null=True, help_text='Your warcry!')
     slug: str = models.SlugField(blank=True, null=True)
+    created_at = models.DateTimeField(datetime.now())
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if not self.slug:
