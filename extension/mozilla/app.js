cedaf82c8257a3cde7acba15f3036c7719e11591
leogutschow <leogusalles@hotmail.com>
2022-09-09 18:44:53 -0300

    Primeira tentativa do Desenvolvimento da Extansão

--- extension/mozilla/app.js
+++ extension/mozilla/app.js
@@ -1,9 +1,33 @@
 console.log("Aqui é o Script de JS");
 console.log("Cristiano Ronaldo criado!");
-var opened = false
-window.open("https://app.roll20.net/editor/");
-opened = true
-setTimeout(40000)
-window.frames.Campaign.characters.create({name: "Leo Teste JS"})
+const chat = document.getElementById("textchat-input");
+const txt = chat.getElementsByTagName("textarea")[0];
+const btn = chat.getElementsByTagName("button")[0];
+const speakingas = document.getElementById("speakingas");
 
+function postChatMessage(message, character = null) {
+    let set_speakingas = true;
+    const old_as = speakingas.value;
+    if (character) {
+        character = character.toLowerCase().trim();
+        for (let i = 0; i < (speakingas.children.length); i++) {
+            if (speakingas.children[i].text.toLowerCase().trim() === character) {
+                speakingas.children[i].selected = true;
+                set_speakingas = false;
+                break;
+            }
+        }
+    }
+    if (set_speakingas)
+        speakingas.children[0].selected = true;
+    const old_text = txt.value;
+    txt.value = message;
+    btn.click();
+    txt.value = old_text;
+    speakingas.value = old_as;
+}
+
+postChatMessage("Teste Extensão")
+
+console.log("Foi até o final")
 //window.Campaign.characters.create({name: "Leonardo"});
