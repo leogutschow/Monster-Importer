fd5483031264027d3b21e4e2de7e3ba5d810553a
leogutschow <leogusalles@hotmail.com>
2022-09-21 14:30:25 -0300

    Colocando o DevLog em uma view

--- devlogs/views.py
+++ devlogs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import UpdateView
+from .models import DevLog
+
 
 # Create your views here.
 class DevLog(UpdateView):
-    pass
+    model = DevLog
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        devlog = self.get_object()
+        context['devlog'] = devlog
+        return context
+
