325421db98dae5dd20af47f44439e7dd346c0680
leogutschow <leogusalles@hotmail.com>
2022-09-23 17:46:35 -0300

    Criando roles dos Profiles

--- authentications/models.py
+++ authentications/models.py
@@ -6,9 +6,16 @@ from django.contrib.auth.models import User, AbstractUser
 
 # Create your models here.
 class Profile(models.Model):
+    roles: list = [
+        ('NEGM', 'New GM'),
+        ('WBGM', 'World Builder GM'),
+        ('MMGM', 'Master Mind GM'),
+        ('GLGM', 'Godlike GM'),
+    ]
     user: int = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     image: str = models.ImageField(upload_to=f'images/profiles/{user}', blank=True, null=True)
     motto: str = models.CharField(max_length=100, blank=True, null=True, help_text='Your warcry!')
+    role: str = models.CharField(max_length=4, choices=roles, default='NEGM')
     slug: str = models.SlugField(blank=True, null=True)
     created_at = models.DateTimeField(timezone.now(), default=timezone.now())
 
