51caddb0d3b68ccf232075b84fcd14e80355b669
leogutschow <leogusalles@hotmail.com>
2022-09-24 13:17:32 -0300

    Começando a criar a View do Index dos Forums

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ForumIndex
+from .views import ForumIndex, ForumCategory
 app_name = 'forum'
 
 urlpatterns = [
-    path('', ForumIndex.as_view(), name='forum_index')
+    path('', ForumIndex.as_view(), name='forum_index'),
+    path('<int:id>', ForumCategory.as_view(), name='category'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Forum
+from .models import Forum, Category
 
 
 # Create your views here.
 class ForumIndex(ListView):
     template_name = 'forum/index.html'
-    model = Forum
+    model = Category
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        categories = Category.objects.all()
+        forums = Forum.objects.none()
+        for category in categories:
+            all_forums = Forum.objects.all()
+            all_forums.filter(category=category)
+            forums = forums.union(all_forums)
+        context['categories'] = categories
+        context['forums'] = forums
+        print(forums)
+        return context
+
+
+class ForumCategory(ListView):
+    template_name = 'forum/forum_category.html'
