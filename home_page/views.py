79c44d028b0252b6cc88351a4da0851cd95a00f7
leogutschow <leogusalles@hotmail.com>
2022-10-13 19:13:51 -0300

    Colocando todos os monstros para aparecerem na Home

--- home_page/views.py
+++ home_page/views.py
@@ -1,22 +1,24 @@
 from django.db.models import QuerySet
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from monsters.models import DnDMonster
+from monsters.models import DnDMonster, BaseSheet
 import random
 
 
 # Create your views here.
 class Index(TemplateView):
-    model = DnDMonster
+    model = BaseSheet
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context: dict = super().get_context_data()
-        monsters: list = DnDMonster.objects.all()
+        monsters: list = BaseSheet.objects.all()
         random_monster = random.randint(1, len(monsters))
-        last_monster: DnDMonster = DnDMonster.objects.get(id=random_monster)
-        context["monsters"]: QuerySet = monsters
-        context['last_monster']: DnDMonster = last_monster
+        last_monster = BaseSheet.objects.get(id=random_monster)
+        if last_monster.image:
+            print(last_monster.image.url)
+        context["monsters"] = monsters
+        context['last_monster'] = last_monster
         return context
 
 
