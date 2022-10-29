803eec80c7341c76d33d434b19fe0f97b49386eb
leogutschow <leogusalles@hotmail.com>
2022-10-29 17:45:33 -0300

    .

--- monsters/models.py
+++ monsters/models.py
@@ -70,9 +70,9 @@ class DnDMonster(BaseSheet):
     languages: str = models.CharField(max_length=100, default="None")
     alignment: str = models.CharField(max_length=30, default="Neutral")
     senses: str = models.CharField(max_length=100, blank=True, null=True)
-    damage_resistances: str = models.CharField(max_length=100, blank=True, null=True)
-    damage_immunities: str = models.CharField(max_length=100, blank=True, null=True)
-    condition_immunities: str = models.CharField(max_length=100, blank=True, null=True)
+    damage_resistances: str = models.CharField(max_length=300, blank=True, null=True)
+    damage_immunities: str = models.CharField(max_length=300, blank=True, null=True)
+    condition_immunities: str = models.CharField(max_length=300, blank=True, null=True)
 
     class Meta:
         verbose_name = "DnD Monster"
