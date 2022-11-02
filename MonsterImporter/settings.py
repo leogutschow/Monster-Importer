a66384bffbee18b2723a0dc98ecf510f29cc1261
leogutschow <leogusalles@hotmail.com>
2022-11-01 21:58:21 -0300

    .

--- MonsterImporter/settings.py
+++ MonsterImporter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-u3a8qo1j3ac(q-y#*ti4b-npx3*hir2p#myd0g%!ezt8$6vj7p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
--- manage.py
+++ manage.py
@@ -3,11 +3,16 @@
 import os
 import sys
 import dotenv
+import pathlib
 
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
+    DOT_ENV_PATH = pathlib.Path() / '.env'
+    if DOT_ENV_PATH.exists():
+        dotenv.read_dotenv(str(DOT_ENV_PATH))
+    else:
+        print('No .env. please create one')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MonsterImporter.settings')
     try:
         from django.core.management import execute_from_command_line
