8785b7afc9ad84448fab85bc55550d92a0470d99
leogutschow <leogusalles@hotmail.com>
2022-11-14 20:27:50 -0300

    Criando um footer, os Termos de Uso e tirando os Manuais por questões legais

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Index, About
+from .views import Index, About, TermsOfUse
 
 app_name = 'home'
 
 urlpatterns = [
     path('', Index.as_view(), name='home_page'),
-    path('about', About.as_view(), name='about')
+    path('about', About.as_view(), name='about'),
+    path('terms', TermsOfUse.as_view(), name='terms')
 ]
